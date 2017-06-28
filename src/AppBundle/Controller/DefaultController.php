9c6ee6faa3e9cfe25027272e61382cb6eacc790e
Jacob Bare <jacob@limit0.io>
2017-06-28 09:21:11 -0500

    Add health check endpoint
    /health-check

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 class DefaultController extends AbstractController
 {
+    public function healthCheckAction()
+    {
+        return new JsonResponse(['data' => ['ok' => true]]);
+    }
 }
